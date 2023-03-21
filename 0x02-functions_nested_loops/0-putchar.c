1c9f3a1d52ff074b902f89a04b995685db230b39
chaimae <aitchelhchaimae@gmail.com>
2023-03-21 13:41:30 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,14 @@
 	 * main - print _putchar
 	 * Return: 0
 	 */
-
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 	int main(void)
-	{
+
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,4 +21,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
