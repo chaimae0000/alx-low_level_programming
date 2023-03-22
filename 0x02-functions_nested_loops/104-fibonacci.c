eacff4d2eae84450f7ab79256fd88d89d7dff37c
chaimae <aitchelhchaimae@gmail.com>
2023-03-21 18:42:11 -0700

    task16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+	/**
+	 * main - fibonacci <3
+	 *
+	 * Purpose - no hardcode
+	 *
+	 * Return:  (Success)
+	 */
+int main(void)
+	{
+	
+
+
+
+
+		}
+		printf("\n");
+		return (0);
+	}
