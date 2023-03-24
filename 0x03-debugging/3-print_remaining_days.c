5205f993dd53d9323b882f68fe11a550b036d2d8
chaimae <aitchelhchaimae@gmail.com>
2023-03-23 19:56:42 -0700

    task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,14 +11,23 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
+int days_remaining;	
 if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
 {
 if (month > 2)
 {
 day++;
 }
+if (day > 60)
+{      
+days_remaining = 366 - day;
+}        
+else
+{
+days_remaining = 365 - day;
+}
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+printf("Remaining days: %d\n", days_remaining);
 }
 else
 {
@@ -28,8 +37,14 @@ printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
+if (day > 365)
+{   
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 365, year);
+}
+else
+{
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
-}
+
