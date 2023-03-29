2849b96dda3d85de8d18a7dae9734d9b5d4fbd56
chaimae <aitchelhchaimae@gmail.com>
2023-03-29 16:00:44 -0700

    task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,55 @@
 #include "main.h"
+
 /**
-  * print_number - Prints any integer with putchar
-  * @n: Number to prints
-  * Return: Nothing
-  */
+ * powten - quick power of ten implementation
+ *
+ * @n: exponent
+ * Return: returns ten to exponent power
+ */
+int function1(int n)
+{
+	int ret;
+
+	ret = 10;
+	while (n > 0)
+	{
+		ret *= 10;
+		n--;
+	}
+	return (ret);
+}
+
+
+/**
+ * print_number - print an integer
+ * @n: first parameter
+ *
+ * Return: 0
+ */
 void print_number(int n)
 {
-	unsigned int x;
+	int s, p;
 
-	if (n < 0)
+	s = 1;
+	p = function1(8);
+	if (n > 0)
 	{
-		_putchar('-');
-		n *= -1;
+		n = n * -1;
+		s = s * -1;
 	}
-
-	x = n;
-
-	if (x / 10)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
+	if (n != 0)
+	{
+		while (n / p == 0)
+			p /= 10;
+		if (s == 1)
+			_putchar('-');
+		while (p >= 1)
+		{
+			_putchar(-(n / p) + '0');
+			n %= p;
+			p /= 10;
+		}
+	}
+	else
+		_putchar('0');
 }
