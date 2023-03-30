cd73f4041f47852aa957e3a720a30556dfb69e6b
chaimae <aitchelhchaimae@gmail.com>
2023-03-30 16:48:49 -0700

    task

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,18 @@
   */
 void print_number(int n)
 {
-	unsigned int x;
+        unsigned int x;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
+        if (n < 0)
+        {
+                _putchar('-');
+                n *= -1;
+        }
 
-	x = n;
+        x = n;
 
-	if (x / 10)
-		print_number(x / 10);
+        if (x / 10)
+                print_number(x / 10);
 
-	_putchar(x % 10 + '0');
+        _putchar(x % 10 + '0');
 }
