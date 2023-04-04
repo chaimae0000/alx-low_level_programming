1e2522a33d0dbdf26254ec3d57b04bd10e9ca1a7
chaimae <aitchelhchaimae@gmail.com>
2023-04-04 14:47:12 -0700

    betty again 20

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,27 +6,26 @@
 * @needle: pointer to char
 * Return: 0
 */
-
 char *_strstr(char *haystack, char *needle)
 {
-    char *result = haystack, *fneedle = needle;
+char *result = haystack, *fneedle = needle;
 
-    while (*haystack)
-    {
-        while (*needle)
-        {
-            if (*haystack++ != *needle++)
-            {
-                break;
-            }
-        }
-        if (!*needle)
-        {
-            return (result);
-        }
-        needle = fneedle;
-        result++;
-        haystack = result;
-    }
-    return (0);
+while (*haystack)
+{
+while (*needle)
+{
+if (*haystack++ != *needle++)
+{
+break;
+}
+}
+if (!*needle)
+{
+return (result);
+}
+needle = fneedle;
+result++;
+haystack = result;
+}
+return (0);
 }
