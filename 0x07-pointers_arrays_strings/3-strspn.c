2a7b54543979eea89f22df61526ecdfdf8a05bb6
chaimae <aitchelhchaimae@gmail.com>
2023-04-04 14:51:24 -0700

    task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,20 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int c = 0;
-    char *t = accept;
+unsigned int c = 0;
+char *t = accept;
 
-    while (*s++)
-    {
-        while (*accept++)
-            if (*(s - 1) == *(accept - 1))
-            {
-                c++;
-                break;
-            }
-        if (!(*--accept))
-            break;
-        accept = t;
-    }
-    return (c);
+while (*s++)
+{
+while (*accept++)
+if (*(s - 1) == *(accept - 1))
+{
+c++;
+break;
+}
+if (!(*--accept))
+break;
+accept = t;
+}
+return (c);
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,21 +7,20 @@
  * @accept: pointer to char
  * Return: NULL
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-    int i;
+int i;
 
-    while (*s)
-    {
-        for (i = 0; accept[i]; i++)
-        {
-            if (*s == accept[i])
-            {
-                return (s);
-            }
-        }
-        s++;
-    }
-    return (NULL);
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+return (s);
+}
+}
+s++;
+}
+return (NULL);
 }
