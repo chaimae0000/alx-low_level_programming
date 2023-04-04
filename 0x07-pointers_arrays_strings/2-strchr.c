0d5591b18f266e0165993a3adace48906f188475
chaimae <aitchelhchaimae@gmail.com>
2023-04-04 14:56:44 -0700

    task edit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,20 +6,19 @@
  * @c: char params to found
  * Return: *S
 */
-
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-        {
-            return (s);
-        }
-        s++;
-    }
-    if (*s == c)
-    {
-        return (s);
-    }
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
 return (0);
 }
