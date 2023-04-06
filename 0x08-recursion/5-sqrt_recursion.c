79c58f6aa0c7693dbfe14718b513edc50efaf45a
chaimae <aitchelhchaimae@gmail.com>
2023-04-05 18:19:05 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,7 +3,7 @@
 /**
 * actual_sqrt_recursion - recurses to find thr natural
 * square root of a number
-* @n: number to calculate the square 
+* @n: number to calculate the square
 * @i: iteratir
 *
 * Return: the resulting square root
@@ -12,12 +12,11 @@ int actual_sqrt_recursion(int n, int i)
 {
 if (i * i > n)
 return (-1);
-if (i *i == n)
+if (i * i == n)
 return (i);
-return (actual_sqrt_recursion(n, i+1));
+return (actual_sqrt_recursion(n, i + 1));
 }
 
- 
 /**
   * _sqrt_recursion - returns the natural square root of a number
   * @n: number to calculate the square root of
