2e0fa77ac19d685ac829fa0aee7fc17a27911f35
chaimae <aitchelhchaimae@gmail.com>
2023-04-12 18:59:01 -0700

    task

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -14,7 +14,8 @@ if (s[c] == ' ')
 flag = 0;
 else if (flag == 0)
 {
-flag = 1; w++;
+flag = 1;
+w++;
 }
 return (w);
 }
@@ -29,7 +30,8 @@ char **strtow(char *str)
 char **matrix, *tmp;
 int i, k = 0, len = 0, words, c = 0, start, end;
 while (*(str + len))
-len++; words = count_word(str);
+len++;
+words = count_word(str);
 if (words == 0)
 return (NULL);
 matrix = (char **) malloc(sizeof(char *) * (words + 1));
@@ -45,7 +47,10 @@ end = i; tmp = (char *) malloc(sizeof(char) * (c + 1));
 if (tmp == NULL)
 return (NULL);
 while (start < end)
-*tmp++ = str[start++]; *tmp = '\0'; matrix[k] = tmp - c; k++; c = 0;
+*tmp++ = str[start++];
+*tmp = '\0'; matrix[k] = tmp - c;
+k++;
+c = 0;
 }
 }
 else if (c++ == 0)
